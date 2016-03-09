1f46799562f9e92f731e9b1bceb6b3c128db7b37
musasabi333 <musasabi333@gmail.com>
2016-03-09 01:06:49 +0100

    Napravljena osnovna forma i kontrole za ucitavanje i osnovno upravljanje ucitanim fajlom

---
+++ MasterAudioTechnologyFunctions/MasterAudioTechnologyFunctions/frmMatf.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace MasterAudioTechnologyFunctions
+{
+    partial class frmMatf
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.pnlTopBar = new System.Windows.Forms.Panel();
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.pnlPlay = new System.Windows.Forms.Panel();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.trbTime = new System.Windows.Forms.TrackBar();
+            this.lblTimeElapsed = new System.Windows.Forms.Label();
+            this.tmrSong = new System.Windows.Forms.Timer(this.components);
+            this.pnlVolume = new System.Windows.Forms.Panel();
+            this.trbVolume = new System.Windows.Forms.TrackBar();
+            this.pnlTopBar.SuspendLayout();
+            this.pnlSideBar.SuspendLayout();
+            this.pnlPlay.SuspendLayout();
+            this.pnlMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbTime)).BeginInit();
+            this.pnlVolume.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenu.Location = new System.Drawing.Point(0, 0);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(30, 30);
+            this.btnMenu.TabIndex = 0;
+            this.btnMenu.Text = "≡";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // pnlTopBar
+            // 
+            this.pnlTopBar.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlTopBar.Controls.Add(this.lblTimeElapsed);
+            this.pnlTopBar.Controls.Add(this.trbTime);
+            this.pnlTopBar.Controls.Add(this.pnlPlay);
+            this.pnlTopBar.Controls.Add(this.btnMenu);
+            this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTopBar.Name = "pnlTopBar";
+            this.pnlTopBar.Size = new System.Drawing.Size(609, 30);
+            this.pnlTopBar.TabIndex = 1;
+            this.pnlTopBar.Leave += new System.EventHandler(this.pnlMenu_Leave);
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.AutoScroll = true;
+            this.pnlSideBar.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlSideBar.Controls.Add(this.pnlVolume);
+            this.pnlSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 30);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(113, 318);
+            this.pnlSideBar.TabIndex = 2;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(0, 0);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(30, 30);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "⏵";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // pnlPlay
+            // 
+            this.pnlPlay.Controls.Add(this.btnLoop);
+            this.pnlPlay.Controls.Add(this.btnStop);
+            this.pnlPlay.Controls.Add(this.btnPlay);
+            this.pnlPlay.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlPlay.Location = new System.Drawing.Point(30, 0);
+            this.pnlPlay.Name = "pnlPlay";
+            this.pnlPlay.Size = new System.Drawing.Size(119, 30);
+            this.pnlPlay.TabIndex = 2;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(30, 0);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(30, 30);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "⏹";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnLoop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoop.Location = new System.Drawing.Point(60, 0);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(30, 30);
+            this.btnLoop.TabIndex = 1;
+            this.btnLoop.Text = "⟲";
+            this.btnLoop.UseVisualStyleBackColor = true;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.Controls.Add(this.btnExit);
+            this.pnlMenu.Controls.Add(this.btnExport);
+            this.pnlMenu.Controls.Add(this.btnImport);
+            this.pnlMenu.Controls.Add(this.btnSaveAs);
+            this.pnlMenu.Controls.Add(this.btnSave);
+            this.pnlMenu.Controls.Add(this.btnOpen);
+            this.pnlMenu.Controls.Add(this.btnNew);
+            this.pnlMenu.Location = new System.Drawing.Point(0, 30);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(230, 163);
+            this.pnlMenu.TabIndex = 3;
+            this.pnlMenu.Leave += new System.EventHandler(this.pnlMenu_Leave);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNew.Location = new System.Drawing.Point(0, 0);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(0);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(230, 23);
+            this.btnNew.TabIndex = 4;
+            this.btnNew.Text = "New Project";
+            this.btnNew.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNew.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOpen.Location = new System.Drawing.Point(0, 23);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(0);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(230, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpen.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSave.Location = new System.Drawing.Point(0, 46);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(230, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save Project";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSaveAs.Location = new System.Drawing.Point(0, 69);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(230, 23);
+            this.btnSaveAs.TabIndex = 7;
+            this.btnSaveAs.Text = "Save Project as";
+            this.btnSaveAs.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSaveAs.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnImport.Location = new System.Drawing.Point(0, 92);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(230, 23);
+            this.btnImport.TabIndex = 8;
+            this.btnImport.Text = "Import WAV/MP3";
+            this.btnImport.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnImport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExport.Location = new System.Drawing.Point(0, 115);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(230, 23);
+            this.btnExport.TabIndex = 9;
+            this.btnExport.Text = "Export as WAV/MP3";
+            this.btnExport.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExit.Location = new System.Drawing.Point(0, 138);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(230, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // trbTime
+            // 
+            this.trbTime.Dock = System.Windows.Forms.DockStyle.Left;
+            this.trbTime.Location = new System.Drawing.Point(149, 0);
+            this.trbTime.Name = "trbTime";
+            this.trbTime.Size = new System.Drawing.Size(104, 30);
+            this.trbTime.TabIndex = 3;
+            this.trbTime.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trbTime.Scroll += new System.EventHandler(this.trbTime_Scroll);
+            // 
+            // lblTimeElapsed
+            // 
+            this.lblTimeElapsed.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblTimeElapsed.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeElapsed.Location = new System.Drawing.Point(253, 0);
+            this.lblTimeElapsed.Name = "lblTimeElapsed";
+            this.lblTimeElapsed.Size = new System.Drawing.Size(112, 30);
+            this.lblTimeElapsed.TabIndex = 4;
+            this.lblTimeElapsed.Text = "00:00:000";
+            this.lblTimeElapsed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmrSong
+            // 
+            this.tmrSong.Tick += new System.EventHandler(this.tmrSong_Tick);
+            // 
+            // pnlVolume
+            // 
+            this.pnlVolume.Controls.Add(this.trbVolume);
+            this.pnlVolume.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlVolume.Location = new System.Drawing.Point(0, 0);
+            this.pnlVolume.Name = "pnlVolume";
+            this.pnlVolume.Size = new System.Drawing.Size(113, 100);
+            this.pnlVolume.TabIndex = 4;
+            // 
+            // trbVolume
+            // 
+            this.trbVolume.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trbVolume.Location = new System.Drawing.Point(0, 0);
+            this.trbVolume.Name = "trbVolume";
+            this.trbVolume.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trbVolume.Size = new System.Drawing.Size(113, 100);
+            this.trbVolume.TabIndex = 4;
+            // 
+            // frmMatf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(609, 348);
+            this.Controls.Add(this.pnlMenu);
+            this.Controls.Add(this.pnlSideBar);
+            this.Controls.Add(this.pnlTopBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frmMatf";
+            this.Text = "Master Audio Technology Functions";
+            this.pnlTopBar.ResumeLayout(false);
+            this.pnlTopBar.PerformLayout();
+            this.pnlSideBar.ResumeLayout(false);
+            this.pnlPlay.ResumeLayout(false);
+            this.pnlMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trbTime)).EndInit();
+            this.pnlVolume.ResumeLayout(false);
+            this.pnlVolume.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbVolume)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.Panel pnlTopBar;
+        private System.Windows.Forms.Panel pnlPlay;
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label lblTimeElapsed;
+        private System.Windows.Forms.TrackBar trbTime;
+        private System.Windows.Forms.Panel pnlVolume;
+        private System.Windows.Forms.TrackBar trbVolume;
+        private System.Windows.Forms.Timer tmrSong;
+    }
+}
+
---
+++ MasterAudioTechnologyFunctions/MasterAudioTechnologyFunctions/frmMatf.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterAudioTechnologyFunctions
+{
+    public partial class frmMatf : Form
+    {
+        private string _productName = "Master Audio Technology Functions";
+        private string _openedFile;
+
+        public frmMatf()
+        {
+            InitializeComponent();
+            pnlMenu.Hide();
+        }
+
+        #region Menu
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            if (pnlMenu.Visible)
+                pnlMenu.Hide();
+            else
+                pnlMenu.Show();
+        }
+
+        private void pnlMenu_Leave(object sender, EventArgs e)
+        {
+            // pnlMenu.Hide();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Waveform audio files (.wav)|*.wav|MATF projects (.matf)|*.matf";
+            dialog.ShowDialog();
+
+            _openedFile = dialog.FileName;
+            Text = _productName + " - " + _openedFile;
+
+            // Set the maximum value of trbTime to the number of miliseconds of the song duration
+
+            pnlMenu.Hide();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion Menu
+
+        #region PlayButtons
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoop_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion PlayButtons
+
+        private void tmrSong_Tick(object sender, EventArgs e)
+        {
+            // Set trbTime to the number of miliseconds that since the start of the song
+            // Set the text of lblTimeElapsed to correct time since the start of the song
+            // Time format: mm:ss:milliseconds
+        }
+
+        private void trbTime_Scroll(object sender, EventArgs e)
+        {
+            // Set the current position of the song to the value of trbTime in milliseconds
+        }
+    }
+}
